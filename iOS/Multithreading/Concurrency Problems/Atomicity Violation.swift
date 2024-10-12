eb18b19166f3bee34bc46e7a5c594d501d61b27b
Arsenty Halimovsky <ghost12@yandex.ru>
2024-10-12 03:45:55 +0300

    Add Atomicity Violation case

--- iOS/Multithreading/Concurrency Problems/Atomicity Violation.swift
+++ iOS/Multithreading/Concurrency Problems/Atomicity Violation.swift
@@ -1,25 +1,27 @@
 // MARK: By this code the operation to increment the count property can lose it's atomic status + Race Condition
 
-import Foundation
+/*
+ import Foundation
 
-var count = 0
+ var count = 0
 
-let thread1 = Thread {
-	for _ in 0...999 {
-		count += 1
-	}
-}
+ let thread1 = Thread {
+ for _ in 0...999 {
+ count += 1
+ }
+ }
 
-let thread2 = Thread {
-	for _ in 0...999 {
-		count += 1
-	}
-}
+ let thread2 = Thread {
+ for _ in 0...999 {
+ count += 1
+ }
+ }
 
-thread1.start()
-thread2.start()
+ thread1.start()
+ thread2.start()
 
-thread1.join() // Wait for thread1 to finish
-thread2.join() // Wait for thread2 to finish
+ thread1.join() // Wait for thread1 to finish
+ thread2.join() // Wait for thread2 to finish
 
-print("Final count: \(count)")
+ print("Final count: \(count)")
+ */
